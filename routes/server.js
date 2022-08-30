28fdd6ba1eae2422fefa3cc0fbbba535c5b3a67a
SBtree-bit <ikimanager99@gmail.com>
2022-08-29 18:11:50 -0700

    a

--- routes/server.js
+++ routes/server.js
@@ -4,18 +4,23 @@ var router = express.Router();
 var games = require("../games.json")
 
 router.get("/query", (req, res) => {
+	res.send("hello you have made a query")
 	if (req.query.all = "true") {
 		res.end(games)
 	} else {
-		let queriedGames = 
+		let queriedGames = []
 		for (var i of Object.keys(games)) {
 			let find = req.query.q
 			if (s.indexOf(find, find.length)) {
-				queriedGames[i] = games[i]
+				queriedGames.push(games[i])
 			}
 		}
 		res.end(queriedGames)
 	}
+})
+
+router.get("/", (req, res) => {
+	res.end("hello this is the server speaking how may i help you")
 })
 
 module.exports = router;
